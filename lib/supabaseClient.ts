3fdf1554b8e74276164d777a41c0e42a5e5b428d
KazuMoment <vencygyro@gmail.com>
2025-11-09 13:07:14 +0800

    reset api keys and hid

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://djfibzmkwajrntsgzovn.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqZmliem1rd2Fqcm50c2d6b3ZuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MjY1Mjg1NiwiZXhwIjoyMDc4MjI4ODU2fQ.-2LT9TP8zLcTQvxDNsmKmFhAouCS8MsKhGET54ndF0c'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
